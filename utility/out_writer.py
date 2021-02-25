1fb081c37159c690f72ade45795b3f27bc6da418
YOUR GITHUB HANDLE <YOUR GITHUB EMAIL>
2021-02-25 20:28:51 +0100

    Fix method usage

--- utility/out_writer.py
+++ utility/out_writer.py
@@ -3,11 +3,12 @@ import os
 
 
 def write_file(input):
+    input_text = convertInput(input)
     base_path = os.path.dirname(os.getcwd())  # get the base directory
     out_path = os.path.join(base_path, 'data', 'out', 'out_file')  # use os.path.join (works better across system)
 
     with open(out_path) as out_file:
-        out_file.write(input) # does this work?
+        out_file.write(input_text) # does this work?
 
 def convertInput(submission):
     #naive approach
