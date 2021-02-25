b687476d2739421ccef012c7056f3ad6fd58fbc8
Lukas Gerhardt <lukas.gerhardt@tum.de>
2021-02-25 18:00:38 +0100

    Add open_file() method

---
+++ script/script.py
@@ -0,0 +1,16 @@
+import os
+
+
+def open_file():
+    base_path = os.path.dirname(os.getcwd())
+    in_path = os.path.join(base_path, 'in', 'a_example')
+
+    with open(in_path) as in_file:
+        content = in_file.read()
+        print(content)
+
+def main():
+    open_file()
+
+if __name__ == '__main__':
+    main()
