6219d8f7595ffcf1b512cd86c1d24bc646983707
annalena94 <annalena.feix@tum.de>
2021-02-25 21:05:57 +0100

    Static test added

--- utility/out_writer.py
+++ utility/out_writer.py
@@ -1,23 +1,32 @@
 # write whatever we decide has to be written out in /data/out/new text file
 import os
+from classes.Submission import Submission
+from classes.Schedule import Schedule
+from classes.Input import Input
 
 
 def write_file(input):
     input_text = convertInput(input)
     base_path = os.path.dirname(os.getcwd())  # get the base directory
-    out_path = os.path.join(base_path, 'data', 'out', 'out_file')  # use os.path.join (works better across system)
+    out_path = os.path.join(base_path, 'data', 'out', 'out_file.txt')  # use os.path.join (works better across system)
 
-    with open(out_path) as out_file:
+    with open(out_path, mode="w") as out_file:
         out_file.write(input_text) # does this work?
 
 def convertInput(submission):
     #naive approach
     submission_text = str(submission.numIntersections) + "\n"
     for schedule in submission.schedules:
-        submission_text += schedule.intersection_id + "\n"
-        submission_text += schedule.numIncomingStreets + "\n"
+        submission_text += str(schedule.intersection_id) + "\n"
+        submission_text += str(schedule.numIncomingStreets) + "\n"
         for street in schedule.street_time:
             submission_text += street + "\n"
     return submission_text
 
 # test
+if __name__ == "__main__":
+    schedule1 = Schedule(2, 1, ["example street 1"])
+    schedule2 = Schedule(3, 2, ["example street 2", "example street 3"])
+    schedules = [schedule1, schedule2]
+    submission = Submission(2, schedules)
+    write_file(submission)
