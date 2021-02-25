cd0cf10c08756f9aaadbd1a963675cc8819e4f6e
Lukas Gerhardt <lukas.gerhardt@tum.de>
2021-02-25 21:30:30 +0100

    update draft 1

--- script/script.py
+++ script/script.py
@@ -3,6 +3,7 @@ from classes import Input
 from classes.Schedule import Schedule
 from classes.Submission import Submission
 
+
 def main():
     # read in the input
     input: Input = in_reader.open_file('a')
@@ -10,25 +11,27 @@ def main():
     # create all schedules
     schedules = []
     for intersection in input.intersections:
+
         # create dictionary to hold street name and time
-        street_time = []
+        street_time = []  # String array
         for street in intersection.incomingStreets:
-            time = str(1) #tbd
-            street_time.append([street, time])
+            time = str(1)  # tbd
+            street_time.append(street + ' ' + time)   # as a String
 
         # create new schedule object
-        newSchedule: Schedule(intersection.id,
-                              intersection.nrIncomingStreets,
-                              street_time)
+        new_schedule = Schedule(intersection.id,
+                                intersection.nrIncomingStreets,
+                                street_time)
         # add new schedule object to schedule array
-        schedules.append(newSchedule)
+        schedules.append(new_schedule)
 
     # create submission object
     num_intersections = len(input.intersections)
-    submission: Submission(num_intersections, schedules)
+    submission = Submission(num_intersections, schedules)
 
     # write the output file in /data
     out_writer.write_file(submission)
+
 
 if __name__ == '__main__':
     main()
